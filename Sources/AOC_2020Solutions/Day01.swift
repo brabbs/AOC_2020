b877604d4fab8cc8f5cab1d22b23439125535bdc
NB6 <nicholas.brabbs@metaswitch.com>
2020-12-01 22:08:14 +0000

    Day 01 Part 1

--- Sources/AOC_2020Solutions/Day01.swift
+++ Sources/AOC_2020Solutions/Day01.swift
@@ -6,14 +6,40 @@
 import Foundation
 
 struct Day01: Solution {
-    let input: String
+    let expenses: [Int]
 
     init(input: String) {
-        self.input = input
+        expenses = input
+            .split(separator: "\n")
+            .compactMap { Int($0) }
+            .sorted()
     }
 
     func first() -> Any {
-        "First answer not yet implemented"
+        var sum = 0
+        // Start with the highest number and the lowest number
+        var lowIndex = expenses.startIndex
+        var highIndex = expenses.endIndex - 1
+
+        let goal = 2020
+        while (sum != goal) {
+            sum = expenses[lowIndex] + expenses[highIndex]
+            print("Total: \(sum), \(lowIndex):\(highIndex)")
+            if sum < goal {
+                // Sum is too low, increase the lower number
+                lowIndex += 1
+            } else if sum > goal {
+                // Sum is too high, decrease the higher number
+                highIndex -= 1
+            }
+
+            if lowIndex == highIndex {
+                // We've met in the middle and not found an exact solution
+                return "No solution found"
+            }
+        }
+
+        return expenses[lowIndex] * expenses[highIndex]
     }
 
     func second() -> Any {
